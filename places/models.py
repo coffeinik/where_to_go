0dbcf8fd0465f6a33525aafa4cc3f1f31427c1ae
Nikita Shamaev <nshamaev@asdco.ru>
2021-12-08 22:20:46 +0300

    Move data from template to view

--- places/models.py
+++ places/models.py
@@ -7,6 +7,8 @@ class Place(models.Model):
     title = models.CharField(max_length=50)
     description_short = models.CharField(max_length=255)
     description_long = models.TextField()
+    coordinates_lng = models.DecimalField(max_digits=9, decimal_places=6)
+    coordinates_lat = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return self.title
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from geojson import FeatureCollection, Feature, Point
 
 
 def index(request):
-    return render(request, 'index.html')
+    feature_collection = FeatureCollection(features=[
+        Feature(geometry=Point((37.62, 55.793676)), properties={
+            'title': '«Легенды Москвы',
+            'placeId': 'moscow_legends',
+            'detailsUrl': 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json'
+          }),
+        Feature(geometry=Point((37.64, 55.753676)), properties={
+            'title': 'Крыши24.рф',
+            'placeId': 'roofs24',
+            'detailsUrl': 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json'
+          }
+        )
+    ])
+    return render(request, 'index.html', context={'feature_collection': feature_collection})
