cdb36b9d2fea2f80621a7ccf11a8e0846c265b68
Nikita Shamaev <nshamaev@asdco.ru>
2021-12-13 19:24:28 +0300

    Change test data in index page to data from database

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse
 from geojson import FeatureCollection, Feature, Point
 
+from places.models import Place
 
 def index(request):
+    places: Place = Place.objects.all()
     feature_collection = FeatureCollection(features=[
-        Feature(geometry=Point((37.62, 55.793676)), properties={
-            'title': '«Легенды Москвы',
-            'placeId': 'moscow_legends',
-            'detailsUrl': 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json'
-          }),
-        Feature(geometry=Point((37.64, 55.753676)), properties={
-            'title': 'Крыши24.рф',
-            'placeId': 'roofs24',
-            'detailsUrl': 'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/roofs24.json'
-          }
-        )
-    ])
+        Feature(
+            geometry=Point((place.coordinates_lng, place.coordinates_lat)),
+            properties={
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': reverse(index) # TODO
+            })
+        for place in places]
+    )
     return render(request, 'index.html', context={'feature_collection': feature_collection})
